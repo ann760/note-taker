a56cda141a10311f56dc9a116b2a4d92808299ab
Ann-Marie <anniedillier@gmail.com>
2020-10-03 02:10:44 -0700

    connect

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 const express = require("express");
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 const { db } = require("./develop/db/db.json");
-
-app.get("/", (req, res) => {
-  res.json(db);
-});
 
 function createNewNote(body, notesArray) {
   const note = body;
@@ -28,13 +24,17 @@ function validateNote(note) {
   return true;
 }
 
-app.post("/api/animals", (req, res) => {
+app.get("/", (req, res) => {
+    res.json(db);
+  });
+
+app.post("/api/db", (req, res) => {
   // set id based on what the next index of the array will be
   req.body.id = note.length.toString();
 
   // if any data in req.body is incorrect, send 400 error back
   if (!validateNote(req.body)) {
-    res.status(400).send("The animal is not properly formatted.");
+    res.status(400).send("The notes is not properly formatted.");
   } else {
     const note = createNewNote(req.body, note);
     res.json(note);
@@ -45,10 +45,10 @@ app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
 
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./public/index.html"));
-// });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
-// app.get("/notes", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./public/notes.html"));
-// });
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
